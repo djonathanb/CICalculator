847da8ff595043048d62e38a03ff125fdb7f1adc
Djonathan Barros <djonathan.barros@gmail.com>
2019-07-05 18:19:14 -0300

    addded unit tests for calculator

---
+++ calc/basics/test_operations.py
@@ -0,0 +1,16 @@
+"""
+Unit tests for the operations library
+"""
+
+from unittest import TestCase
+
+from calc.basics import operations
+
+
+class TestOperations(TestCase):
+
+    def test_addition(self):
+        self.assertEqual(operations.add(2, 2), 4)
+
+    def test_subtraction(self):
+        self.assertEqual(operations.subtract(4, 2), 2)
